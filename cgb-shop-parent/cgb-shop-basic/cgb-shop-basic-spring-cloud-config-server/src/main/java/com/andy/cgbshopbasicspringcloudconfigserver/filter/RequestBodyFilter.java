2cdddde9e91ca1d445bfedb55869b41146605151
yuanxin.gwd <Ccb@123456>
2019-12-27 14:55:06 +0800

    20191227--->spring-cloud-config+kafka+bus+eureka+gitHub:实现高可用分布式配置管理

---
+++ cgb-shop-parent/cgb-shop-basic/cgb-shop-basic-spring-cloud-config-server/src/main/java/com/andy/cgbshopbasicspringcloudconfigserver/filter/RequestBodyFilter.java
@@ -0,0 +1,94 @@
+package com.andy.cgbshopbasicspringcloudconfigserver.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+@Component
+@WebFilter(filterName = "myFilter", urlPatterns = "/*")
+public class RequestBodyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+
+        String url = new String(httpServletRequest.getRequestURI());
+
+        //只过滤/actuator/bus-refresh请求
+        if (!url.endsWith("/bus-refresh")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        //获取原始的body
+        String body = getParm(httpServletRequest);
+
+        //使用HttpServletRequest包装原始请求达到修改post请求中body内容的目的
+        CustometRequestWrapper requestWrapper = new CustometRequestWrapper(httpServletRequest);
+
+        chain.doFilter(requestWrapper, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    /**
+     * 获取请求body
+     * @param httpServletRequest
+     * @return
+     */
+    private String getParm(HttpServletRequest httpServletRequest){
+        String body = "";
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+        InputStream inputStream = null;
+        try {
+            inputStream = httpServletRequest.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        body = stringBuilder.toString();
+        return body;
+    }
+}
--- cgb-shop-parent/cgb-shop-service-impl/src/test/java/com/andy/cgbshopserviceimpl/CgbShopServiceImplApplicationTests.java
+++ cgb-shop-parent/cgb-shop-service-impl/src/test/java/com/andy/cgbshopserviceimpl/CgbShopServiceImplApplicationTests.java
@@ -1,6 +1,6 @@
 package com.andy.cgbshopserviceimpl;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
